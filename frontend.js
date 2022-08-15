cad19a7e7527212df7c91d7c0c9ef12f12714d8b
anupammo <a09051985@gmail.com>
2022-08-15 19:18:17 +0530

    150820221918pm

--- frontend.js
+++ frontend.js
@@ -1,6 +1,26 @@
 // Front end script by anupammondal.in
-    $(document).ready(function () {
-        $("#new_card_dtls").click(function () {
-            $(".collapse").collapse('toggle');
-        });
-    });
+
+function cardanimeOne() {
+    var x = document.getElementById("card-ico-1");
+    var y = document.getElementById("card-ico-2");
+    var z = document.getElementById("card-ico-3");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+}
+function cardanimeTwo() {
+    var x = document.getElementById("card-ico-1");
+    var y = document.getElementById("card-ico-2");
+    var z = document.getElementById("card-ico-3");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+}
+function cardanimeThree() {
+    var x = document.getElementById("card-ico-1");
+    var y = document.getElementById("card-ico-2");
+    var z = document.getElementById("card-ico-3");
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+}
