7adbd65cf5a428148ec588ec3b2a818de888739f
anupammo <a09051985@gmail.com>
2022-08-18 04:11:24 +0530

    170820220411pm

--- frontend.js
+++ frontend.js
@@ -1,13 +1,54 @@
 // Front end script by anupammondal.in
 
-function cardanimeOne() {
-    var x = document.getElementById("card-ico-1");
-    var y = document.getElementById("card-ico-2");
-    var z = document.getElementById("card-ico-3");
-    x.style.display = "none";
-    y.style.display = "block";
-    z.style.display = "none";
-}
+
+$(document).ready(function () {
+    $("#new_card_dtls").focus(function () {
+        $("#card-ico-1").attr("src", "master_card.jpg");
+    });
+    $("#card-cvv").focus(function () {
+        $("#card-ico-1").attr("src", "card_cvv.jpg");
+    });
+    // 
+    $('#new_card_dtls').inputmask({
+        mask: '9999 9999 9999 9999',
+        placeholder: ' ',
+        "oncomplete": function () {
+            $("#card-exp-dt").focus();
+        }
+    });
+    $('#card-exp-dt').inputmask({
+        mask: '99/99',
+        placeholder: ' ',
+        "oncomplete": function () {
+            $("#card-cvv").focus();
+        }
+    });
+    $('#card-cvv').inputmask({
+        mask: '999',
+        placeholder: ' ',
+        "oncomplete": function () {
+            $("#pills-profile-tab").focus();
+        }
+    });
+    $('#card_pin').inputmask({
+        mask: '9{1,4}',
+        jitMasking: true,
+        placeholder: ' '
+    });
+    $('#card_otp').inputmask({
+        mask: '9{1,6}',
+        placeholder: ' '
+    });
+});
+
+// function cardanimeOne() {
+//     var x = document.getElementById("card-ico-1");
+//     var y = document.getElementById("card-ico-2");
+//     var z = document.getElementById("card-ico-3");
+//     x.style.display = "none";
+//     y.style.display = "block";
+//     z.style.display = "none";
+// }
 // function cardanimeTwo() {
 //     var x = document.getElementById("card-ico-1");
 //     var y = document.getElementById("card-ico-2");
