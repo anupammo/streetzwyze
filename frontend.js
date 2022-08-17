d21b249007299c0aea4641d0c8f48a77fa828119
anupammo <a09051985@gmail.com>
2022-08-18 03:51:32 +0530

    170820220351pm

--- frontend.js
+++ frontend.js
@@ -8,31 +8,19 @@ function cardanimeOne() {
     y.style.display = "block";
     z.style.display = "none";
 }
-function cardanimeTwo() {
-    var x = document.getElementById("card-ico-1");
-    var y = document.getElementById("card-ico-2");
-    var z = document.getElementById("card-ico-3");
-    x.style.display = "none";
-    y.style.display = "block";
-    z.style.display = "none";
-}
-function cardanimeThree() {
-    var x = document.getElementById("card-ico-1");
-    var y = document.getElementById("card-ico-2");
-    var z = document.getElementById("card-ico-3");
-    x.style.display = "none";
-    y.style.display = "none";
-    z.style.display = "block";
-}
-function cardanimeFour() {
-    var x = document.getElementById("card-ico-4");
-    var y = document.getElementById("card-ico-5");
-    x.style.display = "none";
-    y.style.display = "block";
-}
-function cardanimeFive() {
-    var x = document.getElementById("card-ico-6");
-    var y = document.getElementById("card-ico-7");
-    x.style.display = "none";
-    y.style.display = "block";
-}
+// function cardanimeTwo() {
+//     var x = document.getElementById("card-ico-1");
+//     var y = document.getElementById("card-ico-2");
+//     var z = document.getElementById("card-ico-3");
+//     x.style.display = "none";
+//     y.style.display = "block";
+//     z.style.display = "none";
+// }
+// function cardanimeThree() {
+//     var x = document.getElementById("card-ico-1");
+//     var y = document.getElementById("card-ico-2");
+//     var z = document.getElementById("card-ico-3");
+//     x.style.display = "none";
+//     y.style.display = "none";
+//     z.style.display = "block";
+// }
